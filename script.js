4c9d0ddea5a8cbabecccc4f618b5eb7ae7534688
irhanftelis <63201597+irhanftelis@users.noreply.github.com>
2024-03-13 18:58:15 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-document.getElementById("rsvp-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    var formData = new FormData(this);
-    var formDataObject = {};
-    formData.forEach(function(value, key){
-        formDataObject[key] = value;
-    });
-    console.log(formDataObject);
+var messages = ["Pacar saya hebat!", "Pacar saya luar biasa!", "Pacar saya adalah yang terbaik!"];
+var currentMessageIndex = 0;
 
-    // Kirim email menggunakan Ajax
-    sendEmail(formDataObject);
-});
-
-function sendEmail(formData) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "backend/send_email.php", true);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                var response = JSON.parse(xhr.responseText);
-                if (response.status === "success") {
-                    alert("Terima kasih atas konfirmasi kehadiran Anda!");
-                } else {
-                    alert("Maaf, terjadi kesalahan. Silakan coba lagi.");
-                }
-            } else {
-                alert("Maaf, terjadi kesalahan. Silakan coba lagi.");
-            }
-        }
-    };
-    xhr.send(JSON.stringify(formData));
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("message").innerText = messages[currentMessageIndex];
 }
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+function okeClicked() {
+    currentMessageIndex = (currentMessageIndex + 1) % messages.length;
+    openPopup();
+}
+
+function noClicked() {
+    openPopup();
+}
+
+// Panggil fungsi openPopup() saat halaman dimuat
+window.onload = openPopup;
